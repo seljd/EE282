24de614a22a12cde85d0c58ae074a84e7b318a57
seljd <55064975+seljd@users.noreply.github.com>
2020-12-10 21:59:11 -0800

    Update chromLengthGCCode.sh

--- code/scripts/chromLengthGCCode.sh
+++ code/scripts/chromLengthGCCode.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 bioawk -c fastx '{ print $name, length($seq), gc($seq) }' < data/processed/dmel-all-chromosome-r6.36_sml100.fasta.gz >output/reports/sml100length.txt
 
 bioawk -c fastx '{ print $name, length($seq), gc($seq) }' < data/processed/dmel-all-chromosome-r6.36_grt100.fasta.gz >output/reports/grt100length.txt
